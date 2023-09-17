05d8a213f6cceb0e5ff22995d60428907910670e
Rinaldo <rinaldod2002@gmail.com>
2023-09-17 15:09:29 -0300

    Criação do projeto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+
+        Console.Write("INFORME O NÚMERO DE TERMOS DESEJADO: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 1)
+        {
+            Console.WriteLine("INFORME UM NÚMERO MAIOR QUE ZERO.");
+        }
+        else
+        {
+            int f1 = 1; int f2 = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(f1);
+                int next_termo = f1 + f2;
+                f1 = f2;
+                f2 = next_termo;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # serie-fibonacci
-Programa para calcular e mostrar os N primeiros termos da série de Fibonacci.
+
+Fazer um programa para calcular e mostrar os N primeiros termos da série de
+Fibonacci. O número N é fornecido pelo usuário. A série de Fibonacci é gerada
+da seguinte forma:
+f1 = f2 = 1;
+f3 = f1 + f2 = 2;
+f4 = f2 + f3 = 3;....
+O primeiro e segundo termos valem 1 e os seguintes são calculados somando
+os dois termos anteriores.
